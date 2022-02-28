4ed4925650de4d08201c10b52c4aa1ec01c7d636
Maisha589 <maisharahman589@gmail.com>
2022-03-01 00:26:04 +0600

    last

--- app.js
+++ app.js
@@ -1,8 +1,74 @@
 const allPlayers = () => {
+    document.getElementById("player-container").innerHTML = "";
+    document.getElementById("spinner").style.display = "block";
     const searchValue = document.getElementById("search-box").value;
+
     const url = `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${searchValue}`;
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data.player[0].strPlayer))
-    console.log(searchValue);
+        .then(data => {
+            if (data.player == null) {
+                document.getElementById("spinner").style.display = "block";
+            }
+            else {
+                showPlayerDetail(data.player);
+                document.getElementById("spinner").style.display = "none";
+            }
+        });
+
+    // console.log(searchValue);
+}
+
+const showPlayerDetail = (players) => {
+    if (!players) {
+        document.getElementById("spinner").style.display = "block";
+    }
+    for (const player of players) {
+        const parent = document.getElementById("player-container");
+
+        const div = document.createElement("div");
+        div.innerHTML = `
+    <div class="card border p-5">
+                    <div class="pro-pic">
+                        <img class="w-50" src="${player.strThumb}" alt="">
+                    </div>
+                    <h2>Name: ${player.strPlayer} </h2>
+                    <h5>Country:${player.strNationality} </h5>
+                    <p></p>
+                    <div class="allbutton">
+                        <button class="btn btn-danger">Delete</button>
+                        <button onclick="details('${player.idPlayer}')" class="btn btn-success">Details</button>
+                    </div>
+                </div>
+    `;
+        parent.appendChild(div);
+        console.log(player);
+    }
+
+    // console.log(player);
+}
+
+const details = (id) => {
+    const url = `https://www.thesportsdb.com/api/v1/json/2/lookupplayer.php?id=${id}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => setDetails(data.players[0]));
+}
+
+const setDetails = (info) => {
+    console.log(info.strGender);
+    if (info.strGender == "Male") {
+        document.getElementById("male").style.display = "block";
+        document.getElementById("female").style.display = "none";
+    }
+    else {
+        document.getElementById("male").style.display = "none";
+        document.getElementById("female").style.display = "block";
+    }
+    document.getElementById("details-container").innerHTML = `
+<div>
+<img class="w-50" src="" alt="">
+<h1>Name: ${info.strPlayer} </h1>
+</div>
+`
 }
